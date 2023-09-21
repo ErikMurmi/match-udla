b3583d8c9ab513caeccb020e23fb8b7e37b25bb6
ErikMurmi <erikmurmi@gmail.com>
2023-09-21 09:59:00 -0500

    Fix: Fixed solicitud import paths

--- pages/gestores/index.js
+++ pages/gestores/index.js
@@ -1,39 +1,39 @@
-import {dbConnect} from '../../../utils/mongoose'
-import { unstable_getServerSession , useSession} from "next-auth/next"
-import { authOptions } from 'pages/api/auth/[...nextauth]'
-import Gestor from 'models/Gestor'
+import { dbConnect } from "utils/mongoose";
+import { unstable_getServerSession, useSession } from "next-auth/next";
+import { authOptions } from "pages/api/auth/[...nextauth]";
+import Gestor from "models/Gestor";
 
-dbConnect()
+dbConnect();
 
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default async function handler(req,res){
-  const {method,body} = req
-  const session = await unstable_getServerSession(req, res, authOptions)
+export default async function handler(req, res) {
+  const { method, body } = req;
+  const session = await unstable_getServerSession(req, res, authOptions);
   //const {status,data} = useSession()
-  console.log('La sesion es '+ session)
+  console.log("La sesion es " + session);
   /*
-    */
-  switch(method){
+   */
+  switch (method) {
     case "GET":
-      try{
-        const gestores = await Gestor.find()
-        return res.status(200).json(gestores)
-      }catch(error){
-        return res.status(500).json({error: error.message})
+      try {
+        const gestores = await Gestor.find();
+        return res.status(200).json(gestores);
+      } catch (error) {
+        return res.status(500).json({ error: error.message });
       }
     case "POST":
-      try{
-        const newGestor = new Gestor(body)
-        const savedGestor = await newGestor.save()
-        return res.status(201).json(savedGestor)
-      }catch(error){
-        return res.status(500).json({error: error.message})
+      try {
+        const newGestor = new Gestor(body);
+        const savedGestor = await newGestor.save();
+        return res.status(201).json(savedGestor);
+      } catch (error) {
+        return res.status(500).json({ error: error.message });
       }
     default:
       return res.status(400).json({
-        msg:'This method does not exits'
-      })
+        msg: "This method does not exits",
+      });
   }
-  console.log(req.method,req.url)
+  console.log(req.method, req.url);
 }
--- pages/public/canchas/index.js
+++ pages/public/canchas/index.js
@@ -1,217 +1,225 @@
 const { useRouter, Router } = require("next/router");
-import { useState, useEffect } from "react"
-import { InstalacionCard } from "components/instalacionCard"
-import Solicitud from "components/solicitud";
+import { useState, useEffect } from "react";
+import { InstalacionCard } from "components/instalacionCard";
+import Solicitud from "components/Solicitud";
 
 export default function Canchas(props) {
-    const { router, query } = useRouter();
-    const [fecha, setFecha] = useState(new Date().toISOString().substring(0, 10))
-    //const [horariosDisponibles,setHorariosDisponibles] = useState([])
-    const [horarioReserva, setHorarioReserva] = useState([])
-    const [desafio, setDesafio] = useState(false)
-    const [reserva, setReserva] = useState({
-        horario: [],
-        usuario: '',
-        fecha: fecha,
-        instalacion: props.Instalacion._id,
-        desafio: desafio
-    })
+  const { router, query } = useRouter();
+  const [fecha, setFecha] = useState(new Date().toISOString().substring(0, 10));
+  //const [horariosDisponibles,setHorariosDisponibles] = useState([])
+  const [horarioReserva, setHorarioReserva] = useState([]);
+  const [desafio, setDesafio] = useState(false);
+  const [reserva, setReserva] = useState({
+    horario: [],
+    usuario: "",
+    fecha: fecha,
+    instalacion: props.Instalacion._id,
+    desafio: desafio,
+  });
 
-    const [showSolcitud, setShowSolicitud] = useState(false)
+  const [showSolcitud, setShowSolicitud] = useState(false);
 
+  // console.log(query)
 
-   // console.log(query)
+  /**
+   * Actualiza el horario de la reserva al iteractuar los checkbox
+   * @param {e} elemento html que llama el metodo
+   */
+  const handleHoraReserva = (e) => {
+    const { value, checked } = e.target;
+    const horario = JSON.parse(value);
+    console.log("checkbox value ", horario);
+    if (checked) {
+      if (horarioReserva.length == 2) {
+        alert("Solo se puede seleccionar máximo dos horas");
+        e.target.checked = false;
+        return;
+      }
+      const primerHorario = horarioReserva[0];
 
+      if (horarioReserva.length == 1 && primerHorario.fin !== horario.inicio) {
+        alert("Solo se puede seleccionar horarios consecutivos");
+        e.target.checked = false;
+        return;
+      }
+      setHorarioReserva([...horarioReserva, horario]);
+      setReserva({ ...reserva, ["horario"]: [...horarioReserva, horario] });
+    } else {
+      let h = Array.from(
+        horarioReserva.filter((item) => item.inicio !== horario.inicio)
+      );
+      setHorarioReserva(h);
+    }
+  };
 
+  /**
+   * Actualiza la fecha de la reserva
+   * @param {e} elemento html que llama el metodo
+   */
+  const handleFechaChange = (e) => {
+    const { value } = e.target;
+    setFecha(new Date(value).toISOString().substring(0, 10));
+  };
+
+  const horariosDisponibles = [
+    {
+      inicio: "07:00",
+      fin: "08:00",
+    },
+    {
+      inicio: "08:00",
+      fin: "09:00",
+    },
+    {
+      inicio: "09:00",
+      fin: "10:00",
+    },
+    {
+      inicio: "10:00",
+      fin: "11:00",
+    },
+    {
+      inicio: "11:00",
+      fin: "12:00",
+    },
+    {
+      inicio: "12:00",
+      fin: "13:00",
+    },
+    {
+      inicio: "13:00",
+      fin: "14:00",
+    },
+    {
+      inicio: "14:00",
+      fin: "15:00",
+    },
+    {
+      inicio: "15:00",
+      fin: "16:00",
+    },
+    {
+      inicio: "16:00",
+      fin: "17:00",
+    },
+    {
+      inicio: "17:00",
+      fin: "18:00",
+    },
+    {
+      inicio: "18:00",
+      fin: "19:00",
+    },
+    {
+      inicio: "19:00",
+      fin: "20:00",
+    },
+  ];
+
+  const handleSubmitReserva = async (form) => {
+    form.preventDefault();
+
+    // const userInfo = await getUserInfo(user)
+    // reserva.usuario = userInfo._id
+    //console.log('userInfo',reserva)
+    if (!horarioReserva.length > 0) {
+      alert("Se debe seleccionar un hora");
+      return;
+    }
+
+    setShowSolicitud(true);
+    reserva.desafio = desafio;
     /**
- * Actualiza el horario de la reserva al iteractuar los checkbox
- * @param {e} elemento html que llama el metodo 
- */
-    const handleHoraReserva = (e) => {
-        const { value, checked } = e.target
-        const horario = JSON.parse(value)
-        console.log('checkbox value ', horario)
-        if (checked) {
-            if( horarioReserva.length == 2){
-                alert("Solo se puede seleccionar máximo dos horas")
-                e.target.checked = false;
-                return
-            }
-            const primerHorario = horarioReserva[0]
+     * Se comprueba si esta abierta a desafio para crear el objeto
+     */
+    // const result = await addReserva(reserva)
 
-            if( horarioReserva.length==1 && (primerHorario.fin !== horario.inicio) ){
-                alert("Solo se puede seleccionar horarios consecutivos")
-                e.target.checked = false;
-                return
-            }  
-            setHorarioReserva([...horarioReserva, horario])
-            setReserva({ ...reserva, ['horario']: [...horarioReserva, horario] })
-        } else {
-            let h = Array.from(horarioReserva.filter((item) => item.inicio !== horario.inicio))
-            setHorarioReserva(h)
-        }
+    // if (result.ok) {
+    //     router.push('/instalaciones')
+    //     alert("reserva realizada")
+    // } else {
+    //     alert('No se pudo crear tu reserva')
+    // }
+  };
 
-    }
-
-    /**
- * Actualiza la fecha de la reserva
- * @param {e} elemento html que llama el metodo 
- */
-    const handleFechaChange = (e) => {
-        const { value } = e.target
-        setFecha(new Date(value).toISOString().substring(0, 10))
-    }
-
-    const horariosDisponibles = [
-        {
-            "inicio": "07:00",
-            "fin": "08:00"
-        },
-        {
-            "inicio": "08:00",
-            "fin": "09:00"
-        },
-        {
-            "inicio": "09:00",
-            "fin": "10:00"
-        },
-        {
-            "inicio": "10:00",
-            "fin": "11:00"
-        },
-        {
-            "inicio": "11:00",
-            "fin": "12:00"
-        },
-        {
-            "inicio": "12:00",
-            "fin": "13:00"
-        },
-        {
-            "inicio": "13:00",
-            "fin": "14:00"
-        },
-        {
-            "inicio": "14:00",
-            "fin": "15:00"
-        },
-        {
-            "inicio": "15:00",
-            "fin": "16:00"
-        },
-        {
-            "inicio": "16:00",
-            "fin": "17:00"
-        },
-        {
-            "inicio": "17:00",
-            "fin": "18:00"
-        },
-        {
-            "inicio": "18:00",
-            "fin": "19:00"
-        },
-        {
-            "inicio": "19:00",
-            "fin": "20:00"
-        }
-    ]
-
-
-    const handleSubmitReserva = async (form) => {
-        form.preventDefault()
-        
-        // const userInfo = await getUserInfo(user)
-        // reserva.usuario = userInfo._id
-        //console.log('userInfo',reserva)
-        if (!horarioReserva.length > 0) {
-            alert("Se debe seleccionar un hora")
-            return
-        }
-
-        setShowSolicitud(true)
-        reserva.desafio = desafio
-        /**
-         * Se comprueba si esta abierta a desafio para crear el objeto
-         */
-        // const result = await addReserva(reserva)
-
-        // if (result.ok) {
-        //     router.push('/instalaciones')
-        //     alert("reserva realizada")
-        // } else {
-        //     alert('No se pudo crear tu reserva')
-        // }
-    }
-
-
-    return (
-        <div className="home">
-            <div className="mid-dis-container">
-                <InstalacionCard instalacion={props.Instalacion}></InstalacionCard>
-                {/* <label htmlFor="desafio">Abrir para desafío</label>
+  return (
+    <div className="home">
+      <div className="mid-dis-container">
+        <InstalacionCard instalacion={props.Instalacion}></InstalacionCard>
+        {/* <label htmlFor="desafio">Abrir para desafío</label>
                         <input type="checkbox" name="desafio" onChange={handleDesafio}></input> */}
-            </div>
-            {/* <form className="mid-dis-container" onSubmit={handleSubmitReserva}>
+      </div>
+      {/* <form className="mid-dis-container" onSubmit={handleSubmitReserva}>
                 <label htmlFor="fecha">Escoje una fecha</label>
                 <input type="date" id="fechaReserva" name="fecha" onChange={handleFechaChange}
                     defaultValue={fecha}></input>
                 <input type="submit" value="Reservar" />
             </form> */}
-            {/* <h3>Horarios disponibles</h3> */}
-            <div className="mid-dis-container">
-                <h3>Escoje una fecha</h3>
-                <form className="mid-dis-container" onSubmit={handleSubmitReserva}>
-                    <input className="fecha-selector" style={{ "alignSelf": "flex-start" }}
-                        type="date" id="fechaReserva" name="fecha" onChange={handleFechaChange}
-                        defaultValue={fecha}></input>
-
-                </form>
-                <h3>Horarios disponibles</h3>
-                <p>Máximo dos horarios consecutivos*</p>
-                <br/>
-                <div className="horarios-container">
-                    {horariosDisponibles.map((horario, index) => (
-                        <div className="horario" key={index}>
-                            <label htmlFor="horario">{`${horario.inicio}-${horario.fin}`}</label>
-                            <input type="checkbox" name="horario" onChange={handleHoraReserva}
-                                value={JSON.stringify(horario)}
-                                title={"horario"} placeholder={"Horario"}
-                            >
-                            </input>
-                        </div>
-                    ))}
-                </div>
-                <h4>Seleccionados</h4>
-                <div className="horarios-container">
-                    {horarioReserva.map((horario, index) => (
-                        <div className="horario-reserva" key={index}>
-                            <label htmlFor="horario">{`${horario.inicio}-${horario.fin}`}</label>
-                            {/* <input type="checkbox" name="horario" onChange={handleHoraReserva}
+      {/* <h3>Horarios disponibles</h3> */}
+      <div className="mid-dis-container">
+        <h3>Escoje una fecha</h3>
+        <form className="mid-dis-container" onSubmit={handleSubmitReserva}>
+          <input
+            className="fecha-selector"
+            style={{ alignSelf: "flex-start" }}
+            type="date"
+            id="fechaReserva"
+            name="fecha"
+            onChange={handleFechaChange}
+            defaultValue={fecha}
+          ></input>
+        </form>
+        <h3>Horarios disponibles</h3>
+        <p>Máximo dos horarios consecutivos*</p>
+        <br />
+        <div className="horarios-container">
+          {horariosDisponibles.map((horario, index) => (
+            <div className="horario" key={index}>
+              <label htmlFor="horario">{`${horario.inicio}-${horario.fin}`}</label>
+              <input
+                type="checkbox"
+                name="horario"
+                onChange={handleHoraReserva}
+                value={JSON.stringify(horario)}
+                title={"horario"}
+                placeholder={"Horario"}
+              ></input>
+            </div>
+          ))}
+        </div>
+        <h4>Seleccionados</h4>
+        <div className="horarios-container">
+          {horarioReserva.map((horario, index) => (
+            <div className="horario-reserva" key={index}>
+              <label htmlFor="horario">{`${horario.inicio}-${horario.fin}`}</label>
+              {/* <input type="checkbox" name="horario" onChange={handleHoraReserva}
                                 value={JSON.stringify(horario)}
                                 title={"horario"} placeholder={"Horario"}
                             >
                             </input> */}
-                        </div>
-                    ))}
-                </div>
-
-                <br />
-                <button type="submit" className="reservar-btn" onClick={(e)=>(handleSubmitReserva(e))}>Reservar</button>
             </div>
-
-            {showSolcitud ?
-                <div className="modal-overlay">
-                    <div className="modal">
-                        <Solicitud show={setShowSolicitud}/>
-                    </div>
-                </div>
-                : null}
+          ))}
         </div>
 
-    )
+        <br />
+        <button
+          type="submit"
+          className="reservar-btn"
+          onClick={(e) => handleSubmitReserva(e)}
+        >
+          Reservar
+        </button>
+      </div>
 
-
+      {showSolcitud ? (
+        <div className="modal-overlay">
+          <div className="modal">
+            <Solicitud show={setShowSolicitud} />
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
 }
 
 // import { getInstalacion} from "controllers/instalacionesController"
@@ -255,15 +263,14 @@ export default function Canchas(props) {
 //         getHorariosDisponibles()
 //     },[fecha])
 
-
 //     // async function getUserInfo(user){
 //     //     const res = await fetch(`http://localhost:3000/api/users/getByFirebaseId?firebase=${user.uid}`)
-//     //     return await res.json() 
+//     //     return await res.json()
 //     // }
 
 //     /**
 //      * Actualiza la fecha de la reserva
-//      * @param {e} elemento html que llama el metodo 
+//      * @param {e} elemento html que llama el metodo
 //      */
 //     const handleFechaChange = (e) => {
 //         const {value} = e.target
@@ -272,7 +279,7 @@ export default function Canchas(props) {
 
 //     /**
 //      * Actualiza el horario de la reserva al iteractuar los checkbox
-//      * @param {e} elemento html que llama el metodo 
+//      * @param {e} elemento html que llama el metodo
 //      */
 //     const handleHoraReserva = (e) => {
 //         const {value,checked} = e.target
@@ -292,7 +299,6 @@ export default function Canchas(props) {
 //         const {checked} = e.target
 //         setDesafio(checked)
 //     }
-
 
 //     const handleSubmitReserva=async(form)=>{
 //         form.preventDefault()
@@ -318,57 +324,55 @@ export default function Canchas(props) {
 //         }
 //     }
 
-
 // }
 
 export async function getServerSideProps({ query: id }) {
-    // const instalacion = await getInstalacion(id)
-    const instalacion = {
-        "horarios": {
-            "lun_vie": {
-                "inicio": "9:00",
-                "fin": "21:00"
-            },
-            "finde": {
-                "inicio": "13:00",
-                "fin": "17:00"
-            },
-            "feriado": {
-                "inicio": "13:00",
-                "fin": "16:00"
-            }
-        },
-        "_id": "6382c706a89ff784a1b3e0c9",
-        "titulo": "Joga Bonito",
-        "deporte": "Fútbol",
-        "precio": 4,
-        "direccion": "Calle Pululahua E2-08 y Pucará",
-        "descripcion": "sintética",
-        "createdAt": "2022-11-27T02:10:14.172Z",
-        "updatedAt": "2023-01-02T20:39:00.141Z",
-        "reservas": [
-            {
-                "horario": [
-                    {
-                        "inicio": "10:00",
-                        "fin": "11:00"
-                    },
-                    {
-                        "inicio": "11:00",
-                        "fin": "12:00"
-                    }
-                ],
-                "abierta": true,
-                "usuario": "63463b29ea2bf5db90490593",
-                "adversario": "",
-                "fecha": "2022-11-30"
-            }]
-    }
-    return (
-        {
-            props: {
-                Instalacion: instalacion,
-            }
-        }
-    )
+  // const instalacion = await getInstalacion(id)
+  const instalacion = {
+    horarios: {
+      lun_vie: {
+        inicio: "9:00",
+        fin: "21:00",
+      },
+      finde: {
+        inicio: "13:00",
+        fin: "17:00",
+      },
+      feriado: {
+        inicio: "13:00",
+        fin: "16:00",
+      },
+    },
+    _id: "6382c706a89ff784a1b3e0c9",
+    titulo: "Joga Bonito",
+    deporte: "Fútbol",
+    precio: 4,
+    direccion: "Calle Pululahua E2-08 y Pucará",
+    descripcion: "sintética",
+    createdAt: "2022-11-27T02:10:14.172Z",
+    updatedAt: "2023-01-02T20:39:00.141Z",
+    reservas: [
+      {
+        horario: [
+          {
+            inicio: "10:00",
+            fin: "11:00",
+          },
+          {
+            inicio: "11:00",
+            fin: "12:00",
+          },
+        ],
+        abierta: true,
+        usuario: "63463b29ea2bf5db90490593",
+        adversario: "",
+        fecha: "2022-11-30",
+      },
+    ],
+  };
+  return {
+    props: {
+      Instalacion: instalacion,
+    },
+  };
 }
